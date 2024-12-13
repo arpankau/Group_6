3540fa40bf8898670ed7e49e2ed034aefb0f3bc9
Gourab Podder <ghostv22@gmail.com>
2024-12-13 13:50:34 -0600

    checking prevalages

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.outbox}>
+        <Text>Open up App.js to start working on ssyour app!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
